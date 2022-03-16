a345bc7b7c032e22d5722662680cb9c9c174b8c3
felo <felix.kurgat@student.moringaschool.com>
2022-03-16 17:55:07 +0300

    added class prodev

---
+++ app/config.py
@@ -0,0 +1,28 @@
+import os
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Felixkurgat5@localhost/blogs'
+    #email configurations
+    MAIL_SERVER = 'smtp.google.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    # simple mde configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Felixkurgat5@localhost/blogs'
+    DEBUG = True
+class TestConfig(Config):
+    DEBUG = True
+config_options = {
+  'development':DevConfig,
+  'production':ProdConfig,
+  'test':TestConfig
+}
+
