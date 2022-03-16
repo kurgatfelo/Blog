2009d2425b00e32ffb207301bc5fb31518b85261
felo <felix.kurgat@student.moringaschool.com>
2022-03-16 17:56:13 +0300

    added class testconfig

--- app/config.py
+++ app/config.py
@@ -15,10 +15,10 @@ class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
-
+class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Felixkurgat5@localhost/blogs'
     DEBUG = True
-class TestConfig(Config):
+
     DEBUG = True
 config_options = {
   'development':DevConfig,
