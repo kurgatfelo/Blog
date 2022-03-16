af87f48c057057136710472bc30c10fbf8e7f7b4
felo <felix.kurgat@student.moringaschool.com>
2022-03-16 17:55:44 +0300

    added class devconfig

--- app/config.py
+++ app/config.py
@@ -11,11 +11,11 @@ class Config:
     # simple mde configurations
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
-
+class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
-class DevConfig(Config):
+
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Felixkurgat5@localhost/blogs'
     DEBUG = True
 class TestConfig(Config):
